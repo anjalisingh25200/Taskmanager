b87db95d6b9606a0b529d1cbd5d06030524a57af
anjalisingh2520 <anjalisingh200025@gmail.com>
2022-03-05 23:14:22 +0530

    all validation and Hashing is done

--- db/modals/usersmodal.js
+++ db/modals/usersmodal.js
@@ -1,31 +1,43 @@
 const mongoose = require('mongoose')
 const validator = require('validator');
+const { hash } = require('bcryptjs')
 const UsersSchema = new mongoose.Schema({
     Username: {
 
         type: String,
         required: true,
-        minlength: 1,
         trim: true
     },
     Password: {
 
         type: String,
         required: true,
-        minlength: 1,
-        trim: true
+        minlength: 8,
+        trim: true,
+
+        validate(value) {
+            if (value.toLowerCase().includes('Password')) {
+                throw new Error('password cannot contain"Password"')
+            }
+        }
+
     },
     Email: {
 
         type: String,
         required: true,
-        minlength: 1,
+        lowercase: true,
         trim: true,
         validate(val) {
             if (!validator.isEmail(val)) {
                 throw new Error("Email is invalid");
             }
         }
+    }
+})
+UsersSchema.pre('save', async function () {
+    if (this.isModified('Password')) {
+        this.Password = await hash(this.Password, 12)
     }
 })
 //now we create a module
